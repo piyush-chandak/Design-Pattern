f3027d75405f4f107e46468e5a5c66caa8085e64
Piyush Chandak <chandakpiyush4@gmail.com>
2021-12-30 17:06:50 +0530

    Adapter pattern

---
+++ Structural Pattern/Adapter.ts
@@ -0,0 +1,12 @@
+// Adapter Pattern
+// - Structural Design Pattern
+// - Allows incompatible class to interact with each other by converting interface of one class to another class
+
+// Usage
+// - When class(not having an interface) need to reuse existence code.
+
+
+// Reference
+// - https://refactoring.guru/design-patterns/adapter
+// - https://www.dofactory.com/javascript/design-patterns/adapter
+// - https://www.youtube.com/watch?v=BMfc_oPwp3U
